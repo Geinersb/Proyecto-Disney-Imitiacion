b0e7abeb532335a0ac606b812d9861be011c6871
Geinersb <geinersb20@gmail.com>
2025-04-17 17:39:44 -0600

    se finaliza el slider principal

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+//CHANGE IMG 
+function changeImg(anything) {
+    // document.querySelector(".img-user-perfil").src = anything;
+    document.querySelector(".img-user").src = anything;
+}
+
+
+
+
+
+//CHANGE TEXT
+const nameuser = document.querySelector(".name-user");
+
+function changeText(text) {     
+nameuser.innerHTML = text;
+
+
+}
